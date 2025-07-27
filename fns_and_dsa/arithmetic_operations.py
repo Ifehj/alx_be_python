64a73fdf596c1dc40814111de22914abc6d86ab6
Adesanya Ifeoluwa <Adesanya7ife9@gmail.com>
2025-07-27 18:55:12 +0100

    refactor code

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,17 @@
+# arithmetic_operations.py
+
 def perform_operation(num1: float, num2: float, operation: str):
-	match operation:
-		case "add":
-			return num1 + num2
-		case "subtract":
-			return num1 - num2
-		case "multiply":
-			return num1 * num2	
-		case "divide":					
-			if num2 == 0:
-				return "Error: Division by zero is not allowed."
-			return num1 / num2
-		case _:
-			return "Error: Invalid operation. Please choose from addition, subtraction, multiplication, or division."
+    
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Error: Division by zero"
+        else:
+            return num1 / num2
+    else:
+        return "Error: Invalid operation"
