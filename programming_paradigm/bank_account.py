8a1d6496223ce1ee38d8a3687541f6ad305a87c6
Adesanya Ifeoluwa <Adesanya7ife9@gmail.com>
2025-07-31 20:42:40 +0100

    refactor

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,26 +1,27 @@
 class BankAccount:
-	def __init__(self, account_balance, initial_balance=0):
+	def __init__(self, initial_balance=0):
 		"""Initialize the bank account with an initial balance."""
-		self.account_balance = account_balance
-		self.initial_balance = initial_balance
+		self.__account_balance = initial_balance
 
 	def deposit(self, amount):
 		"""Deposit a specified amount into the account."""
 		if amount > 0:
-			self.account_balance += amount
+			self.__account_balance += amount
 			print(f"Deposited: {amount}")
 		else:
 			print("Deposit amount must be positive.")
 	
 	def withdraw(self, amount):
 		"""Withdraw a specified amount from the account."""
-		if amount > 0 and amount <= self.account_balance:
-			self.account_balance -= amount
+		if amount > 0 and amount <= self.__account_balance:
+			self.__account_balance -= amount
 			print(f"Withdrew: {amount}")
+			return True
 		else:
 			print("Insufficient funds")
+			return False
 
 
 	def display_balance(self):
 		"""Display the current account balance."""
-		print(f"Current balance: ${self.account_balance}")
+		print(f"Current Balance: ${self.__account_balance}")
