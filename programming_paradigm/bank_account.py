d428080dc96515e712134a25d046a0ab3c93c5c4
Adesanya Ifeoluwa <Adesanya7ife9@gmail.com>
2025-07-31 20:20:50 +0100

    add back account file

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,26 @@
+class BankAccount:
+	def __init__(self, account_balance, initial_balance=0):
+		"""Initialize the bank account with an initial balance."""
+		self.account_balance = account_balance
+		self.initial_balance = initial_balance
+
+	def deposit(self, amount):
+		"""Deposit a specified amount into the account."""
+		if amount > 0:
+			self.account_balance += amount
+			print(f"Deposited: {amount}")
+		else:
+			print("Deposit amount must be positive.")
+	
+	def withdraw(self, amount):
+		"""Withdraw a specified amount from the account."""
+		if amount > 0 and amount <= self.account_balance:
+			self.account_balance -= amount
+			print(f"Withdrew: {amount}")
+		else:
+			print("Insufficient funds")
+
+
+	def display_balance(self):
+		"""Display the current account balance."""
+		print(f"Current balance: {self.account_balance}")
