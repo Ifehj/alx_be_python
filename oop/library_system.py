5abf34bcf79b9e8ff8fbd4053c255e73c9171b92
Adesanya Ifeoluwa <Adesanya7ife9@gmail.com>
2025-08-07 16:46:14 +0100

    refactor

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,3 +1,5 @@
+# library_system.py
+
 class Book:
     def __init__(self, title, author):
         self.title = title
@@ -24,13 +26,12 @@ class Library:
         self.books.append(book)
 
     def list_books(self):
-        def list_books(self):
-            for book in self.books:
-                if isinstance(book, EBook):
-                    print(
-                        f"EBook: {book.title} by {book.author}, File Size: {book.file_size}KB")
-                elif isinstance(book, PrintBook):
-                    print(
-                        f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
-                else:
-                    print(f"Book: {book.title} by {book.author}")
+        for book in self.books:
+            if isinstance(book, EBook):
+                print(
+                    f"EBook: {book.title} by {book.author}, File Size: {book.file_size}KB")
+            elif isinstance(book, PrintBook):
+                print(
+                    f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
+            else:
+                print(f"Book: {book.title} by {book.author}")
